b88410b698240c44db93191a027a8d4077ea8e39
Alisson Araujo de Sousa <alissonaraujo20042018@gmail.com>
2025-02-14 20:48:51 -0300

    Arquivo Readme preenchido

--- pax_admin.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-# Configuração da página
-st.set_page_config(page_title="Funerária Pax",
-                   page_icon="✝️",
-                   layout="centered")
-st.title("Notas de Falecimento Salvas")
-
-
